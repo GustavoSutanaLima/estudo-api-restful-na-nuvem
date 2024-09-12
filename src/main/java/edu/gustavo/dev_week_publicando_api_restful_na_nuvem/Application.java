f48f8051f494be5310c0c194e1ed8eca2b8bdc49
Gustavo Sutana <gustavosutanalima@gmail.com>
2024-09-12 09:32:11 -0300

    Commit inicial: projeto maven criado, com as devidas dependências;

---
+++ src/main/java/edu/gustavo/dev_week_publicando_api_restful_na_nuvem/Application.java
@@ -0,0 +1,13 @@
+package edu.gustavo.dev_week_publicando_api_restful_na_nuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/gustavo/dev_week_publicando_api_restful_na_nuvem/DevWeekPublicandoApiRestfulNaNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.gustavo.dev_week_publicando_api_restful_na_nuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevWeekPublicandoApiRestfulNaNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
