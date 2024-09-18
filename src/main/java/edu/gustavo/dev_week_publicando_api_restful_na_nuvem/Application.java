8bb0baca729c3a5cd1b14231a9e137739f061ca4
Gustavo Sutana <gustavosutanalima@gmail.com>
2024-09-18 07:45:29 -0300

    Hotfix: CORS

--- src/main/java/edu/gustavo/dev_week_publicando_api_restful_na_nuvem/Application.java
+++ src/main/java/edu/gustavo/dev_week_publicando_api_restful_na_nuvem/Application.java
@@ -3,6 +3,11 @@ package edu.gustavo.dev_week_publicando_api_restful_na_nuvem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+//Correção de erro CORS
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "URL Padrão de Servidor")})
 @SpringBootApplication
 public class Application {
 
